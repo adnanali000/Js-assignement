276cbf0cdcf59012bbde719716511cf9fdbd3f7d
Adnan musil <adnan.museel123@gmail.com>
2020-06-11 18:30:02 +0500

    js assignment chp 1-20 deploy

--- app.js
+++ app.js
@@ -1,8 +1,67 @@
 // CHAPTER 1
 
-alert("Error! Please enter a valid password.");
-alert("Welcome to JS Land... \n Happy Coding!");
-alert("Welcome to JS Land...");
-alert("Happy Coding!");
+// alert("Error! Please enter a valid password.");
+// alert("Welcome to JS Land... \n Happy Coding!");
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// chapter2
+
+//1
+var username;
+//2
+var myName = "Adnan Musil";
+//3
+var message = "Hello World";
+alert(message);
+
+//4
+var name = "Jhone Doe";
+var age = "15 years old";
+var qualification = "Certified Mobile Application Development";
+alert(name);
+alert(age);
+alert(qualification);
+
+//5
+var pizza = "PIZZA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" + "\n" + "P";
+alert(pizza);
+
+//6
+var email = "adnan.museel123@gmail.com";
+alert("hey email address is " + email);
+
+//7
+var book = "A smarter way to learn Javascript";
+alert("I am trying to learn from the book "+book);
+
+//8
+document.write("Yah! I can write HTML content through Javascript");
+
+//9
+var x = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(x);
+document.write(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
