e28bec794c8945cfc33ed843c48d5f425e62084a
Adnan musil <adnan.museel123@gmail.com>
2020-06-17 21:49:16 +0500

    js assignment chp 1-20 eploy

--- app.js
+++ app.js
@@ -227,78 +227,304 @@
 // document.write("you will need "+eatTotal+" wavy to last you until the ripe old age of "+maxAge+"<br>");
 
 
-// chapter 6-9
+// // chapter 6-9
 
-// task1
-var a = 10;
-document.write("The value of ++a is: "+ ++a +"<br>");
-document.write("now the value of a is: "+ a + "<br>");
+// // task1
+// var a = 10;
+// document.write("The value of ++a is: "+ ++a +"<br>");
+// document.write("now the value of a is: "+ a + "<br>");
 
-document.write("The value of a++ is: "+ a++ + "<br>");
-document.write("now the value of a is: "+ a +"<br>");
+// document.write("The value of a++ is: "+ a++ + "<br>");
+// document.write("now the value of a is: "+ a +"<br>");
 
-document.write("The value of --a is: "+ --a +"<br>");
-document.write("now the value of a is: "+ a + "<br>");
+// document.write("The value of --a is: "+ --a +"<br>");
+// document.write("now the value of a is: "+ a + "<br>");
 
-document.write("The value of a-- is: "+ a-- +"<br>");
-document.write("now the value of a is: "+ a + "<br>");
-
-
-// task2
-var a = 2;
-var b = 1;
-var result = --a - --b + ++b + b--; 
-//--a; 1
-//--a - --b; 1-0 = 1
-//--a - --b + ++b; 1-0+1 = 2
-//--a - --b + ++b + b--; 1-0+1+1 = 3
-document.write("The value of result: "+ result + "<br>");//3
-document.write("The value of a: "+a+"<br>");//1
-document.write("The value of b: "+b+"<br>");//0
-
-// task3
-
-var user = prompt("Enter your name");
-alert("Welcome "+user);
-
-//task4 //missing question
-
-//task5
-var five = 5;
-var table = prompt("enter number",five);
-document.write("Table of "+table+"<br>");
-document.write(table+"* "+"1 "+"= "+table*1+"<br>");
-document.write(table+"* "+"2 "+"= "+table*2+"<br>");
-document.write(table+"* "+"3 "+"= "+table*3+"<br>");
-document.write(table+"* "+"4 "+"= "+table*4+"<br>");
-document.write(table+"* "+"5 "+"= "+table*5+"<br>");
-document.write(table+"* "+"6 "+"= "+table*6+"<br>");
-document.write(table+"* "+"7 "+"= "+table*7+"<br>");
-document.write(table+"* "+"8 "+"= "+table*8+"<br>");
-document.write(table+"* "+"9 "+"= "+table*9+"<br>");
-document.write(table+"* "+"10 "+"= "+table*10+"<br>");
+// document.write("The value of a-- is: "+ a-- +"<br>");
+// document.write("now the value of a is: "+ a + "<br>");
 
 
-// task6
+// // task2
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--; 
+// //--a; 1
+// //--a - --b; 1-0 = 1
+// //--a - --b + ++b; 1-0+1 = 2
+// //--a - --b + ++b + b--; 1-0+1+1 = 3
+// document.write("The value of result: "+ result + "<br>");//3
+// document.write("The value of a: "+a+"<br>");//1
+// document.write("The value of b: "+b+"<br>");//0
 
-    var sub1 = prompt("Enter first subject name");
-    var sub2 = prompt("Enter second subject name");
-    var sub3 = prompt("Enter third subject name");
-    var marksTotal = 100;
-    var sub1Marks = Number(prompt("Enter obtained marks of "+sub1))
-    var sub2Marks = Number(prompt("Enter obtained marks of "+sub2))
-    var sub3Marks = Number(prompt("Enter obtained marks of "+sub3))
-    var sub1Percentage = (sub1Marks * 100) / marksTotal;
-    var sub2Percentage = (sub2Marks * 100) / marksTotal;
-    var sub3Percentage = (sub3Marks * 100) / marksTotal; 
+// // task3
 
-    var totalAdd = 300;
-    var obtainedAdd = sub1Marks + sub2Marks + sub3Marks;
-    var totalPercentage = (obtainedAdd * 100)/totalAdd;
+// var user = prompt("Enter your name");
+// alert("Welcome "+user);
 
-    document.write("Subject"+" "+"Total Marks"+" "+"Obtained Marks"+" "+"Percentage"+"<br>");
-    document.write(sub1+"--------"+marksTotal+"-----------"+sub1Marks+"-----------------------"+sub1Percentage+"%"+"<br>");
-    document.write(sub2+"--------"+marksTotal+"-----------"+sub2Marks+"-----------------------"+sub2Percentage+"%"+"<br>");
-    document.write(sub3+"--------"+marksTotal+"-----------"+sub3Marks+"------------------------"+sub3Percentage+"%"+"<br>");
-    document.write("Total"+"--------"+totalAdd+"-----------"+obtainedAdd+"----------------------"+totalPercentage+"%"+"<br>");
+// //task4 //missing question
+
+// //task5
+// var five = 5;
+// var table = prompt("enter number",five);
+// document.write("Table of "+table+"<br>");
+// document.write(table+"* "+"1 "+"= "+table*1+"<br>");
+// document.write(table+"* "+"2 "+"= "+table*2+"<br>");
+// document.write(table+"* "+"3 "+"= "+table*3+"<br>");
+// document.write(table+"* "+"4 "+"= "+table*4+"<br>");
+// document.write(table+"* "+"5 "+"= "+table*5+"<br>");
+// document.write(table+"* "+"6 "+"= "+table*6+"<br>");
+// document.write(table+"* "+"7 "+"= "+table*7+"<br>");
+// document.write(table+"* "+"8 "+"= "+table*8+"<br>");
+// document.write(table+"* "+"9 "+"= "+table*9+"<br>");
+// document.write(table+"* "+"10 "+"= "+table*10+"<br>");
+
+
+// // task6
+
+//     var sub1 = prompt("Enter first subject name");
+//     var sub2 = prompt("Enter second subject name");
+//     var sub3 = prompt("Enter third subject name");
+//     var marksTotal = 100;
+//     var sub1Marks = Number(prompt("Enter obtained marks of "+sub1))
+//     var sub2Marks = Number(prompt("Enter obtained marks of "+sub2))
+//     var sub3Marks = Number(prompt("Enter obtained marks of "+sub3))
+//     var sub1Percentage = (sub1Marks * 100) / marksTotal;
+//     var sub2Percentage = (sub2Marks * 100) / marksTotal;
+//     var sub3Percentage = (sub3Marks * 100) / marksTotal; 
+
+//     var totalAdd = 300;
+//     var obtainedAdd = sub1Marks + sub2Marks + sub3Marks;
+//     var totalPercentage = (obtainedAdd * 100)/totalAdd;
+
+//     document.write("Subject"+" "+"Total Marks"+" "+"Obtained Marks"+" "+"Percentage"+"<br>");
+//     document.write(sub1+"--------"+marksTotal+"-----------"+sub1Marks+"-----------------------"+sub1Percentage+"%"+"<br>");
+//     document.write(sub2+"--------"+marksTotal+"-----------"+sub2Marks+"-----------------------"+sub2Percentage+"%"+"<br>");
+//     document.write(sub3+"--------"+marksTotal+"-----------"+sub3Marks+"------------------------"+sub3Percentage+"%"+"<br>");
+//     document.write("Total"+"--------"+totalAdd+"-----------"+obtainedAdd+"----------------------"+totalPercentage+"%"+"<br>");
    
+
+// // chapter 9-11
+
+// // task1
+
+// var city = prompt("Enter your city");
+// if(city == "Karachi"){
+
+//     alert("Welcome to the city of lights");
+// }
+
+// // task2
+// var gender = prompt("Enter your gender");
+// if(gender == "male"){
+//     alert("Good morning sir");
+// }
+// else if(gender == "female"){
+//     alert("Good morning Ma'am");
+// }
+// else{
+//     alert("Enter your gender")
+// }
+
+// //task3
+// var signalColour = prompt("Enter signal colour");
+// if(signalColour == "Red"){
+//     alert("Must Stop");
+// }
+// else if(signalColour == "Yellow"){
+//     alert("Ready to move");
+// }
+// else if(signalColour == "Green"){
+//     alert("Move Now");
+// }
+
+
+// // task4
+// var fuel = Number(prompt("Enter your fuel in litres"));
+// if(fuel < 0.25){
+//     alert("Please refil the fuel in your car");
+// }
+// else{
+//     alert("Okay");
+// }
+
+// //task5
+// //a
+// var a = 4;
+// if(++a === 5){
+//     alert("given condition for a variable a is true");
+// }
+// else{
+//     alert("false")
+// }
+
+// //b
+// var b = 82;
+// if(b++ === 83){
+//     alert("given conditon for a variable b is true")
+// }
+// else{
+//     alert("false")
+// }
+
+// //c
+// var c = 12;
+// if(c++ === 13){
+//     alert("condition 1 is true");
+// }
+// if(c === 13){
+//     alert("condtion 2 is true");
+// }
+// if(++c < 14){
+//     alert("condition 3 is true");
+// }
+// if(c === 14){
+//     alert("condition 4 is true");
+// }
+
+// //d
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if(totalCost == laborCost + materialCost){
+//     alert("The cost equals")
+// }
+// else{
+//     alert("Not equal")
+// }
+
+
+// //e
+// if(true){
+//     alert("True");
+// }
+// if(false){
+//     alert("False");
+// }
+
+// //f
+// if("car" < "cat"){
+//     alert("car is less than cat");
+// }
+
+
+// // task6
+
+// document.write("Marks Sheet"+"<br>");
+
+// var subject1Marks = Number(prompt("Enter subject 1 marks"));
+// var subject2Marks = Number(prompt("Enter subject 2 marks"));
+// var subject3Marks = Number(prompt("Enter subject 3 marks"));
+// var marksTotal = 300;
+// var subjectsTotal = subject1Marks + subject2Marks + subject3Marks;
+// var percent = (subjectsTotal * 100) / marksTotal;
+
+// if(percent >= 80){
+//     document.write("Total Marks: "+marksTotal+"<br>");
+//     document.write("Marks obtained: "+subjectsTotal+"<br>");
+//     document.write("Percentage: "+percent+"%"+"<br>");
+//     document.write("Grade: A-one"+"<br>");
+//     document.write("Remarks: Excellent"+"<br>");
+// }
+// else if(percent >= 70){
+//     document.write("Total Marks: "+marksTotal+"<br>");
+//     document.write("Marks obtained: "+subjectsTotal+"<br>");
+//     document.write("Percentage: "+percent+"%"+"<br>");
+//     document.write("Grade: A"+"<br>");
+//     document.write("Remarks: Good"+"<br>");
+// }
+// else if(percent >= 60){
+//     document.write("Total Marks: "+marksTotal+"<br>");
+//     document.write("Marks obtained: "+subjectsTotal+"<br>");
+//     document.write("Percentage: "+percent+"%"+"<br>");
+//     document.write("Grade: B"+"<br>");
+//     document.write("Remarks: You need to improve"+"<br>");
+// }
+// else if(percent < 60){
+//     document.write("Total Marks: "+marksTotal+"<br>");
+//     document.write("Marks obtained: "+subjectsTotal+"<br>");
+//     document.write("Percentage: "+percent+"%"+"<br>");
+//     document.write("Grade: Fail"+"<br>");
+//     document.write("Remarks: Sorry"+"<br>");
+// }
+
+// // task 7
+
+// var secretNum = 4;
+// var userNum = Number(prompt("Guess the secret number between 1-10"));
+// if(secretNum === userNum){
+//     alert("Bingo! correct answer");
+// }
+// else if(userNum === 3){
+//     alert("close enough to the correct answer")
+// }
+
+
+// // task 8
+
+// var divisibleBy3 = 9;
+// if(divisibleBy3 % 3 === 0){
+//     alert("Number is divisible by 3")
+// }
+// else{
+//     alert("not divisible by 3")
+// }
+
+// // task 9 
+
+// var userInput = Number(prompt("enter number"));
+// if(userInput % 2 === 0){
+//     alert("even");
+// }
+// else if(userInput % 2 !== 0){
+//     alert("odd");
+// }
+// else{
+//     alert("enter correct number")
+// }
+
+// // task 10
+
+// var temperature = prompt("Enter temperature");
+// if(temperature > 40){
+//     alert("It is too hot outside")
+// }
+// else if(temperature > 30){
+//     alert("The weather today is normal")
+// }
+// else if(temperature > 20){
+//     alert("Today's weather is cool");
+// }
+// else if(temperature > 10){
+//     alert("OMG! Today's weather is so cool")
+// }
+// else{
+//     alert("enter temperature")
+// }
+
+// // task 11
+
+// var firstNum = Number(prompt("Enter first number"));
+// var secondNum = Number(prompt("Enter second number"));
+// var operator =  prompt("Enter operator","+,-,*,/,%");
+
+// if(operator === "+"){
+//     document.write(firstNum + "+" + secondNum + "=" + (firstNum+secondNum));
+
+// }
+// if(operator === "-"){
+//     document.write(firstNum + "-" + secondNum + "=" + (firstNum-secondNum));
+
+// }
+// if(operator === "*"){
+//     document.write(firstNum + "*" + secondNum + "=" + (firstNum*secondNum));
+
+// }
+// if(operator === "/"){
+//     document.write(firstNum + "/" + secondNum + "=" + (firstNum/secondNum));
+
+// }
+// if(operator === "%"){
+//     document.write(firstNum + "%" + secondNum + "=" + (firstNum%secondNum));
+
+// }
